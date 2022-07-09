5d022c0cccc0ad59ef14b76dbc26cb277e8f72b2
robdll <dilillo.roberto@gmail.com>
2022-07-09 18:57:34 +0100

    adds ticker reducer

---
+++ src/state/reducers/ticker.js
@@ -0,0 +1,13 @@
+const initialState = {
+  foo: "bar",
+};
+
+const ticker = (state = initialState, action) => {
+  switch (action.type) {
+    default: {
+      return state;
+    }
+  }
+};
+
+export default ticker;
