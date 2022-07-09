b2acf887564becb9c4cdc415eec42425616f4822
robdll <dilillo.roberto@gmail.com>
2022-07-09 17:50:46 +0100

    update reducer with new actions

--- src/state/reducers/orderbook.js
+++ src/state/reducers/orderbook.js
@@ -1,12 +1,30 @@
-import { TEST_ACTION } from "../actions";
+import {
+  INIT_ORDERBOOK,
+  UPDATE_ORDERBOOK,
+  EDIT_ORDERBOOK_PRECISION,
+} from "../actions";
 
 const initialState = {
-  foo: "bar",
+  bids: [],
+  asks: [],
+  precision: "P0",
 };
 
 const orderbook = (state = initialState, action) => {
   switch (action.type) {
-    case TEST_ACTION: {
+    case INIT_ORDERBOOK: {
+      console.log("init", action.payload);
+      return {
+        ...state,
+      };
+    }
+    case UPDATE_ORDERBOOK: {
+      console.log("update", action.payload);
+      return {
+        ...state,
+      };
+    }
+    case EDIT_ORDERBOOK_PRECISION: {
       return {
         ...state,
       };
